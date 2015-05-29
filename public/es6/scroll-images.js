3fffa33337c71e9481a8b93fc57b303f94a5d404
Pedro Sland <pedro_sland@hotmail.com>
2015-05-29 15:18:04 +0100

    Fixed scrolling with multiple items

--- public/es6/scroll-images.js
+++ public/es6/scroll-images.js
@@ -4,10 +4,12 @@
         constructor($parent){
             this.speed = 2;
 
-            this.$parent = $parent;
+            this.$parent = $parent.wrap('<div class="scroll-bg--wrapper"></div>');
             this.$container = $('<div class="scroll-bg">');
             this.$containerFront = $('<div class="scroll-bg--front">');
             this.$containerBack = $('<div class="scroll-bg--back">');
+
+            this.$spacer = $('<div class="scroll-bg--spacer"></div>');
 
             this.currentIndex = -1;
         }
@@ -23,9 +25,15 @@
 
             var viewportHeight = $(window).height();
 
-            this.$parent.css('margin-top', viewportHeight / this.speed);
+            this.$parent.children().height(viewportHeight);
+
+            //this.$spacer.height(viewportHeight / this.speed);
+            // this.$parent.css('margin-top', $(window).height() / this.speed);
 
             this.numPanes = this.$parent.children().length;
+
+            this.$parent.parent().css('height', $(window).height() * this.numPanes / this.speed)
+                .css('overflow', 'hidden');
 
             this.requestRender();
         }
@@ -54,6 +62,17 @@
 
             this.currentIndex = index;
 
+            if(index === 0){
+                this.$parent.css('margin-top', $(window).height() / this.speed);
+            }else{
+                this.$parent.css('margin-top', 0);
+            }
+            //this.$parent.css('margin-top', $(window).height() / this.speed - ($(window).height() / this.speed * index));
+
+            this.$parent.height($(window).height() * this.numPanes / this.speed + $(window).height() / this.speed);
+
+            //this.$spacer.insertBefore('.pane-' + (index + 1));
+
             // Move new front image to front
             this.$parent.find('.pane-' + (index + 1))
                 .appendTo(this.$containerFront);
@@ -62,12 +81,20 @@
         handleAnimationFrame(){
             var viewportHeight = $(window).height();
 
-            var percentage = $(window).scrollTop() / viewportHeight;
+            var scrollTop = $(window).scrollTop();
+
+            // scrollTop -= parseInt(this.$parent.css('margin-top'));
+
+            var percentage = scrollTop / viewportHeight * this.speed;
             var index = Math.floor(percentage);
+
+            console.log('index %s, percentage %s, panes %s', index, percentage, this.numPanes);
+
+            //this.$parent.css('margin-top', $(window).height() / this.speed * index);
 
             var viewportPercentage = percentage - index;
 
-            this.$containerFront.css('top', -viewportPercentage * this.speed * viewportHeight);
+            this.$containerFront.css('top', -viewportPercentage * viewportHeight);
 
             if(this.currentIndex !== index){
                 this.setFrontImage(index);
