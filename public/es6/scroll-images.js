c1e192c9157c3a571d81d487367cc415498b17b0
Pedro Sland <pedro_sland@hotmail.com>
2015-05-29 15:21:26 +0100

    Some cleanup

--- public/es6/scroll-images.js
+++ public/es6/scroll-images.js
@@ -9,26 +9,17 @@
             this.$containerFront = $('<div class="scroll-bg--front">');
             this.$containerBack = $('<div class="scroll-bg--back">');
 
-            this.$spacer = $('<div class="scroll-bg--spacer"></div>');
-
             this.currentIndex = -1;
         }
 
         init(){
             this.initListeners();
 
-            // this.$parent.children().appendTo(this.$containerBack);
-
-            // this.$container.append(this.$containerBack, this.$containerFront);
-            // this.$parent.append(this.$container);
             this.$containerFront.insertBefore(this.$parent);
 
             var viewportHeight = $(window).height();
 
             this.$parent.children().height(viewportHeight);
-
-            //this.$spacer.height(viewportHeight / this.speed);
-            // this.$parent.css('margin-top', $(window).height() / this.speed);
 
             this.numPanes = this.$parent.children().length;
 
@@ -67,11 +58,8 @@
             }else{
                 this.$parent.css('margin-top', 0);
             }
-            //this.$parent.css('margin-top', $(window).height() / this.speed - ($(window).height() / this.speed * index));
 
             this.$parent.height($(window).height() * this.numPanes / this.speed + $(window).height() / this.speed);
-
-            //this.$spacer.insertBefore('.pane-' + (index + 1));
 
             // Move new front image to front
             this.$parent.find('.pane-' + (index + 1))
@@ -83,14 +71,8 @@
 
             var scrollTop = $(window).scrollTop();
 
-            // scrollTop -= parseInt(this.$parent.css('margin-top'));
-
             var percentage = scrollTop / viewportHeight * this.speed;
             var index = Math.floor(percentage);
-
-            console.log('index %s, percentage %s, panes %s', index, percentage, this.numPanes);
-
-            //this.$parent.css('margin-top', $(window).height() / this.speed * index);
 
             var viewportPercentage = percentage - index;
 
