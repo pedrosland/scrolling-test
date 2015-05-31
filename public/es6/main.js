35b318d54a2a0e07603a4e56cd732e438e0b8f9c
Pedro Sland <pedro_sland@hotmail.com>
2015-05-31 15:35:46 +0100

    Added hack to make links behave better for a fake site

--- public/es6/main.js
+++ public/es6/main.js
@@ -1,3 +1,10 @@
 $(function(){
     $('.scroll-panes').scrollImages();
+
+    // Hack since this is a demo. Makes links nicer.
+    $('a').click(function(e){
+        if($(this).attr('href') === '#'){
+            e.preventDefault();
+        }
+    });
 });
