1a5d1e3b55f53f7426343212c4f54b3267e579d2
Pedro Sland <pedro_sland@hotmail.com>
2015-05-31 15:55:08 +0100

    Add styles and js for sign up button

--- public/es6/main.js
+++ public/es6/main.js
@@ -2,9 +2,17 @@ $(function(){
     $('.scroll-panes').scrollImages();
 
     // Hack since this is a demo. Makes links nicer.
-    $('a').click(function(e){
+    $('body').on('click', 'a', function(e){
         if($(this).attr('href') === '#'){
             e.preventDefault();
         }
+    });
+
+    $('.sign-up').one('click', function(e){
+        e.preventDefault();
+
+        $(this)
+            .addClass('sign-up--completed')
+            .text('Thanks! You’ve signed up');
     });
 });
